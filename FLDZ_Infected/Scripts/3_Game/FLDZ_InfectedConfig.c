2b64c95ed5f2cfbe38e2e367a10013c8a4db2abb
C0staGe0 <127120744+C0staGe0@users.noreply.github.com>
2023-07-06 14:30:15 +0100

    Update FLDZ_InfectedConfig.c

--- FLDZ_Infected/Scripts/3_Game/FLDZ_InfectedConfig.c
+++ FLDZ_Infected/Scripts/3_Game/FLDZ_InfectedConfig.c
@@ -1,6 +1,6 @@
 class FLDZ_InfectedConfig
 {
-    private static const string CONFIG_VERSION = "0.0.2";
+    private static const string CONFIG_VERSION = "0.0.3";
     private static const string FLDZ_InfectedModFolder = "$profile:\\FLDZ_Infected\\";
     private static const string FLDZ_InfectedConfigName = "FLDZ_InfectedConfig.json";
 
@@ -10,7 +10,8 @@ class FLDZ_InfectedConfig
     int InfectedSpawnDelayAfterStartInMinutes = 1; // Delay in minutes after server start to enable infected spawn system if EnableInfectedSpawn = 1
     int InfectedSpawnAmount = 10; // Amount of infected to spawn
     int SpawnRadius = 1000; // Radius in meters around the center of the map to spawn infected
-    int CanSpawnContainer = 1; // 1 = Enabled, 0 = Disabled - If the loot container will be able to spawn with the horde
+    bool CanSpawnContainer = 1; // 1 = Enabled, 0 = Disabled - If the loot container will be able to spawn with the horde
+    bool CanSpawnWorldObject = 1; // 1 = Enabled, 0 = Disabled - If the world object will be able to spawn with the horde
     autoptr TStringArray InfectedClassNames_1 = {
         "ZmbM_priestPopSkinny",
         "ZmbM_SkaterYoung_Grey",
@@ -35,64 +36,7 @@ class FLDZ_InfectedConfig
         "ZmbM_HermitSkinny_Red",
         "ZmbM_HermitSkinny_Green",
         "ZmbM_HermitSkinny_Black",
-        "ZmbM_HermitSkinny_Beige",
-		"ZmbM_FarmerFat_Green",
-        "ZmbM_FarmerFat_Brown",
-        "ZmbM_FarmerFat_Blue",
-        "ZmbM_FarmerFat_Beige",
-		"ZmbM_ClerkFat_White",
-        "ZmbM_ClerkFat_Khaki",
-        "ZmbM_ClerkFat_Grey",
-        "ZmbM_ClerkFat_Brown",
-        "ZmbM_CitizenBFat_Red",
-        "ZmbM_CitizenBFat_Green",
-        "ZmbM_CitizenBFat_Blue",
-        "ZmbM_CitizenASkinny_Red",
-        "ZmbM_CitizenASkinny_Grey",
-        "ZmbM_CitizenASkinny_Brown",
-        "ZmbM_CitizenASkinny_Blue",
-        "ZmbF_VillagerOld_White",
-        "ZmbF_VillagerOld_Red",
-        "ZmbF_VillagerOld_Green",
-        "ZmbF_VillagerOld_Blue",
-        "ZmbF_SurvivorNormal_White",
-        "ZmbF_SurvivorNormal_Red",
-        "ZmbF_SurvivorNormal_Orange",
-        "ZmbF_SurvivorNormal_Blue",
-        "ZmbF_SkaterYoung_Violet",
-        "ZmbF_SkaterYoung_Striped",
-        "ZmbF_SkaterYoung_Brown",
-        "ZmbF_ShortSkirt_yellow",
-        "ZmbF_ShortSkirt_white",
-        "ZmbF_ShortSkirt_stripes",
-        "ZmbF_ShortSkirt_red",
-        "ZmbF_ShortSkirt_grey",
-        "ZmbF_ShortSkirt_green",
-        "ZmbF_ShortSkirt_checks",
-        "ZmbF_ShortSkirt_brown",
-        "ZmbF_ShortSkirt_black",
-        "ZmbF_ShortSkirt_beige",
-		"ZmbF_JoggerSkinny_Red",
-        "ZmbF_JoggerSkinny_Green",
-        "ZmbF_JoggerSkinny_Brown",
-        "ZmbF_JoggerSkinny_Blue",
-        "ZmbF_HikerSkinny_Red",
-        "ZmbF_HikerSkinny_Grey",
-        "ZmbF_HikerSkinny_Green",
-        "ZmbF_HikerSkinny_Blue",
-        "ZmbF_DoctorSkinny",
-        "ZmbF_Clerk_Normal_White",
-        "ZmbF_Clerk_Normal_Red",
-        "ZmbF_Clerk_Normal_Green",
-        "ZmbF_Clerk_Normal_Blue",
-        "ZmbF_CitizenBSkinny",
-        "ZmbF_CitizenANormal_Brown",
-        "ZmbF_CitizenANormal_Blue",
-        "ZmbF_CitizenANormal_Beige",
-        "ZmbF_BlueCollarFat_White",
-        "ZmbF_BlueCollarFat_Red",
-        "ZmbF_BlueCollarFat_Green",
-        "ZmbF_BlueCollarFat_Blue"
+        "ZmbM_HermitSkinny_Beige"
     };
     autoptr TStringArray InfectedClassNames_2 = {
         "ZmbM_PrisonerSkinny",
@@ -127,38 +71,7 @@ class FLDZ_InfectedConfig
         "ZmbM_FishermanOld_Green",
         "ZmbM_FishermanOld_Blue",
         "ZmbM_FirefighterNormal",        
-        "ZmbM_DoctorFat",
-        "ZmbM_ConstrWorkerNormal_Grey",
-        "ZmbM_ConstrWorkerNormal_Green",
-        "ZmbM_ConstrWorkerNormal_Black",
-        "ZmbM_ConstrWorkerNormal_Beige",
-        "ZmbM_CommercialPilotOld_Olive",
-        "ZmbM_CommercialPilotOld_Grey",
-        "ZmbM_CommercialPilotOld_Brown",
-        "ZmbM_CommercialPilotOld_Blue",        
-        "ZmbF_PoliceWomanNormal",
-        "ZmbF_PatientOld",
-        "ZmbF_ParamedicNormal_Red",
-        "ZmbF_ParamedicNormal_Green",
-        "ZmbF_ParamedicNormal_Blue",
-        "ZmbF_NurseFat",
-        "ZmbF_MilkMaidOld_Grey",
-        "ZmbF_MilkMaidOld_Green",
-        "ZmbF_MilkMaidOld_Black",
-        "ZmbF_MilkMaidOld_Beige",
-        "ZmbF_MechanicNormal_Orange",
-        "ZmbF_MechanicNormal_Grey",
-        "ZmbF_MechanicNormal_Green",
-        "ZmbF_MechanicNormal_Beige",
-        "ZmbF_JournalistNormal_White",
-        "ZmbF_JournalistNormal_Red",
-        "ZmbF_JournalistNormal_Green",
-        "ZmbF_JournalistNormal_Blue",        
-        "ZmbM_SoldierNormal",
-        "ZmbM_PatrolNormal_Autumn",
-        "ZmbM_PatrolNormal_Flat",
-        "ZmbM_PatrolNormal_PautRev",
-        "ZmbM_PatrolNormal_Summer"
+        "ZmbM_DoctorFat"
     };
     autoptr TStringArray ContainerObject = {
         "SeaChest",
@@ -193,6 +106,13 @@ class FLDZ_InfectedConfig
         "Ammo_45ACP",
         "TunaCan",
         "RDG5Grenade"
+    };
+    autoptr TStringArray WorldObject = {
+        "Land_Wreck_C130J",
+        "Wreck_Mi8_Crashed",
+        "Wreck_Mi8",
+        "Wreck_UH1Y",
+        "Land_Wreck_Caravan_MRust"
     };
 
     void Load()
